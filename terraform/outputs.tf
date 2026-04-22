3ca30003df7ef7ca493edcf54f747b22a661c191
ananomri <ananomrii@gmail.com>
2026-04-22 08:50:50 +0100

    fix change tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,5 @@
 # ============================================
-# OUTPUTS — Affichés après terraform apply
+# OUTPUTS — Infos affichées après terraform apply
 # ============================================
 
 output "instance_public_ip" {
@@ -7,34 +7,14 @@ output "instance_public_ip" {
   value       = aws_instance.tp_api.public_ip
 }
 
-output "frontend_url" {
-  description = "🎓 URL de l'application (frontend + API)"
-  value       = "http://${aws_instance.tp_api.public_ip}:${var.app_port}"
-}
-
 output "api_url" {
-  description = "URL de l'API"
-  value       = "http://${aws_instance.tp_api.public_ip}:${var.app_port}/api"
-}
-
-output "health_url" {
-  description = "Endpoint de health check"
-  value       = "http://${aws_instance.tp_api.public_ip}:${var.app_port}/health"
+  description = "URL de votre API"
+  value       = "http://${aws_instance.tp_api.public_ip}:${var.app_port}"
 }
 
 output "ssh_command" {
   description = "Commande pour se connecter en SSH"
   value       = "ssh -i ${var.private_key_path} ec2-user@${aws_instance.tp_api.public_ip}"
-}
-
-output "view_bootstrap_logs" {
-  description = "Commande pour voir les logs du bootstrap"
-  value       = "ssh -i ${var.private_key_path} ec2-user@${aws_instance.tp_api.public_ip} 'sudo tail -f /var/log/cloud-init-output.log'"
-}
-
-output "view_app_logs" {
-  description = "Commande pour voir les logs de l'app (PM2)"
-  value       = "ssh -i ${var.private_key_path} ec2-user@${aws_instance.tp_api.public_ip} 'pm2 logs academie'"
 }
 
 output "instance_id" {
